eff5c27beefd7024677ccd1dd3c509dcf6029148
yong <ahyong@gmail.com>
2015-12-09 01:03:58 -0800

    change to new format

--- eventbrite.py
+++ eventbrite.py
@@ -49,7 +49,7 @@ response = requests.get(
 '''with open(filename, 'a+') as  output_file:
 			output_file.write(response_event.json()["events"][j]["name"]["text"]+'\n')'''
 
-for i in range (response.json()["pagination"]["object_count"]):
+'''for i in range (response.json()["pagination"]["object_count"]):
 	user_profile=response.json()['attendees'][i]['profile']
 	answers = response.json()['attendees'][i]['answers']
 	status= response.json()['attendees'][i]['barcodes'][0]['status']
@@ -75,11 +75,12 @@ for i in range (response.json()["pagination"]["object_count"]):
 		str='%d,%s,%s.%s,%s\n' %(i+1, user_profile['name'], user_profile['email'], wechat_id['answer'], hobbies['answer'])
 	except :
 		print i+1, user_profile['name'], user_profile['email'], first_time['answer'], add_quote(first_attend['answer']), status, date
-		str='%d,%s,%s\n' % (i+1, user_profile['name'], user_profile['email'])
-'''for i in range (response.json()["pagination"]["object_count"]):
+		str='%d,%s,%s\n' % (i+1, user_profile['name'], user_profile['email'])'''
+for i in range (response.json()["pagination"]["object_count"]):
 	user_profile=response.json()['attendees'][i]['profile']
 	answers = response.json()['attendees'][i]['answers']
 	status= response.json()['attendees'][i]['barcodes'][0]['status']
+	est_time = response.json()['attendees'][i]['barcodes'][0]['created']
 	first_time=answers[3]
 	wechat_id = answers[4]
 	hobbies = answers[5]
@@ -89,18 +90,20 @@ for i in range (response.json()["pagination"]["object_count"]):
 	first_attend =answers[9]
 	live_place = answers[10]
 	where = answers[1]
+	utc_date = dateutil.parser.parse(est_time)
+	date = utc_to_local(utc_date)
 
 	try :
 		wechat_id['answer']
 		hobbies['answer']
 		print i+1, user_profile['name'].decode('utf-8'), user_profile['email'], wechat_id['answer'], first_time['answer'], \
 		      '"'+hobbies['answer'].replace(',', ' ').rstrip()+'"', '"'+books['answer'].replace(',', ' ').rstrip()+'"', company['answer'], \
-		      add_quote(position['answer']), add_quote(live_place['answer']), where['answer'], status
+		      add_quote(position['answer']), add_quote(live_place['answer']), status, date
 		str='%d,%s,%s.%s,%s\n' %(i+1, user_profile['name'], user_profile['email'], wechat_id['answer'], hobbies['answer'])
 	except :
-		print i+1, user_profile['name'], user_profile['email'], first_time['answer'], add_quote(first_attend['answer']), where['answer'], status
+		print i+1, user_profile['name'], user_profile['email'], first_time['answer'], add_quote(first_attend['answer']), status, date
 		str='%d,%s,%s\n' % (i+1, user_profile['name'], user_profile['email'])
 
 
 	with open(filename, 'a+') as  output_file:
-			output_file.write(str)'''
+			output_file.write(str)
