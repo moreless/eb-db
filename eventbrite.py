aada95a542a8e19e7c0b0e80640d524d60825766
yong <ahyong@gmail.com>
2016-01-15 01:14:13 -0800

    using json format

--- eventbrite.py
+++ eventbrite.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python
 # coding: utf-8
+
 import requests
 import sys
 import re
@@ -10,23 +11,81 @@ from datetime import datetime, timedelta
 #import pymongo
 #from pymongo import MongoClient
 
+def findAnswers(answers):
+  ans=['','','','','','','','','']
+  '''
+    This funciton is used to find the wechatID, hobby, company and job.
+  '''
+  
+  if not answers:
+    print 'Errors! No answers found in this entry.\n'
+    return 
+  
+  for entry in answers:
+
+    if re.search('Where did you hear ', entry['question'], re.I):
+      if 'answer' in entry:
+        ans[0] = entry['answer']
+        #print where
+    elif re.search('First Time Coming', entry['question'], re.I):
+      if 'answer' in entry:
+        ans[1] = entry['answer']
+        #print first_time
+    if re.search('wechat ID', entry['question'], re.I):
+      if 'answer' in entry:
+        ans[2] = entry['answer']
+        #print wechat_id
+    elif re.search('Hobbies', entry['question'], re.I):
+      if 'answer' in entry:
+        ans[3] = entry['answer']
+
+    elif re.search('Categroies of Books', entry['question'], re.I):
+      if 'answer' in entry:
+        ans[4] = entry['answer'] 
+
+    elif re.search('Company', entry['question'], re.I):
+      if 'answer' in entry:
+        ans[5] = entry['answer']
+
+    elif re.search('Position', entry['question'], re.I):
+      if 'answer' in entry:
+        ans[6] = entry['answer']
+
+    elif re.search('was the first time', entry['question'], re.I):
+      if 'answer' in entry:
+        ans[7] = entry['answer'] 
+
+    elif re.search('The city you live', entry['question'], re.I):
+      if 'answer' in entry:
+        ans[8] = entry['answer']
+
+
+  #return where, first_time, wechat_id, hobbies, books, company, position, first_attend, live_place
+  return ans
+
 def get_register_data(response, i, filename, flag):
-    user_profile = response.json()['attendees'][i]['profile']
-    answers = response.json()['attendees'][i]['answers']
-    status = response.json()['attendees'][i]['barcodes'][0]['status']
-    est_time = response.json()['attendees'][i]['barcodes'][0]['created']
-    first_time = answers[3]
-    wechat_id = answers[4]
-    hobbies = answers[5]
-    books = answers[6]
-    company = answers[7]
-    position = answers[8]
-    first_attend = answers[9]
-    live_place = answers[10]
-    where = answers[1]
+    dataEntry    = response.json()['attendees'][i]
+  
+    user_profile = dataEntry['profile']
+    answers      = dataEntry['answers']
+    status       = dataEntry['barcodes'][0]['status']
+    est_time     = dataEntry['barcodes'][0]['created']
+
     utc_date = dateutil.parser.parse(est_time)
     date = utc_to_local(utc_date)
-    
+    ans = findAnswers(answers)
+    #print ans
+
+    where= ans[0]
+    first_time = ans[1]
+    wechat_id = ans[2]
+    hobbies = ans[3]
+    books= ans[4]
+    company = ans[5]
+    position = ans[6]
+    first_attend =ans[7]
+    live_place = ans [8]
+
     if 'answer' in hobbies:
       hobbies_str = hobbies['answer'].replace(',', ' ').rstrip()
     else:
@@ -61,19 +120,17 @@ def get_register_data(response, i, filename, flag):
 
     if flag:
       i=i+50
-    try :
-        wechat_id['answer']
-        hobbies['answer']
-        print i + 1, user_profile['name'].decode('utf-8'), user_profile['email'], wechat_id['answer'], first_time['answer'], \
-              '"' + hobbies['answer'].replace(',', ' ').rstrip() + '"', '"' + books['answer'].replace(',', ' ').rstrip() + '"', company['answer'], \
-              add_quote(position['answer']), add_quote(live_place['answer']), where['answer'], status, date
-        str = '%d,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % (i + 1, user_profile['name'].decode('utf-8'), user_profile['email'], wechat_id['answer'], first_time['answer'], \
-            hobbies['answer'].replace(',', ' ').rstrip(), books['answer'].replace(',', ' ').rstrip(), company['answer'], position['answer'], \
-            live_place['answer'], where['answer'], status, date)
-    except :
-        print i + 1, user_profile['name'], user_profile['email'], first_time['answer'], add_quote(first_attend['answer']), where['answer'], status, date
-        str = '%d,%s,%s.%s,%s,%s,%s,%s\n' % (i + 1, user_profile['name'], user_profile['email'], first_time['answer'], first_attend['answer'].decode('utf-8'), \
-         where['answer'], status, date)
+    if (first_time == 'Yes'):
+        print i + 1, user_profile['name'].decode('utf-8'), user_profile['email'], wechat_id, first_time, \
+              '"' + hobbies.replace(',', ' ').rstrip() + '"', '"' + books.replace(',', ' ').rstrip() + '"', company, \
+              add_quote(position), add_quote(live_place), where, status, date
+        str = '%d,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % (i + 1, user_profile['name'].decode('utf-8'), user_profile['email'], wechat_id, first_time, \
+            hobbies.replace(',', ' ').rstrip(), books.replace(',', ' ').rstrip(), company, position, \
+            live_place, where, status, date)
+    else :
+        print i + 1, user_profile['name'], user_profile['email'], first_time, add_quote(first_attend), where, status, date
+        str = '%d,%s,%s.%s,%s,%s,%s,%s\n' % (i + 1, user_profile['name'], user_profile['email'], first_time, first_attend.decode('utf-8'), \
+         where, status, date)
     with open(filename, 'a') as  output_file:
         output_file.write(str)
 
@@ -102,14 +159,14 @@ response_event = requests.get(
 )
 for j in range(response_event.json()["pagination"]["object_count"]):
     pass
-print response_event.json()["events"][j-1]["name"]["text"]
+print response_event.json()["events"][j]["name"]["text"]
 
 # mo= re.findall(u'(第.+?期)', str(response_event.json()["events"][j]["name"]["text"]))
 # print mo[0]
 
 response = requests.get(
     # "https://www.eventbriteapi.com/v3/users/me/owned_events/",
-    "https://www.eventbriteapi.com/v3/events/" + response_event.json()["events"][j-1]["id"] + "/attendees/",
+    "https://www.eventbriteapi.com/v3/events/" + response_event.json()["events"][j]["id"] + "/attendees/",
     headers={
         "Authorization": "Bearer 76IZEIMV6O2VAOYUCKOE",
     },
@@ -119,7 +176,7 @@ response = requests.get(
 if (response.json()["pagination"]["object_count"]>50):
   response2 = requests.get(
     # "https://www.eventbriteapi.com/v3/users/me/owned_events/",
-    "https://www.eventbriteapi.com/v3/events/" + response_event.json()["events"][j-1]["id"] + "/attendees/?page=2",
+    "https://www.eventbriteapi.com/v3/events/" + response_event.json()["events"][j]["id"] + "/attendees/?page=2",
     headers={
         "Authorization": "Bearer 76IZEIMV6O2VAOYUCKOE",
     },
@@ -132,35 +189,7 @@ db = client['ValleyRain']
 collection = db['UserProfile']'''
     
 with open(filename, 'a') as  output_file:
-            output_file.write(response_event.json()["events"][j-1]["name"]["text"] + '\n')
-
-'''for i in range (response.json()["pagination"]["object_count"]):
-    user_profile=response.json()['attendees'][i]['profile']
-    answers = response.json()['attendees'][i]['answers']
-    status= response.json()['attendees'][i]['barcodes'][0]['status']
-    est_time = response.json()['attendees'][i]['barcodes'][0]['created']
-    first_time=answers[2]
-    wechat_id = answers[3]
-    hobbies = answers[4]
-    books = answers[5]
-    company = answers[6]
-    position = answers[7]
-    first_attend =answers[8]
-    live_place = answers[9]
-    where = answers[10]
-    utc_date = dateutil.parser.parse(est_time)
-    date = utc_to_local(utc_date)
-
-    try :
-        wechat_id['answer']
-        hobbies['answer']
-        print i+1, user_profile['name'].decode('utf-8'), user_profile['email'], wechat_id['answer'], first_time['answer'], \
-              '"'+hobbies['answer'].replace(',', ' ').rstrip()+'"', '"'+books['answer'].replace(',', ' ').rstrip()+'"', company['answer'], \
-              add_quote(position['answer']), add_quote(live_place['answer']), where['answer'], status, date
-        str='%d,%s,%s.%s,%s\n' %(i+1, user_profile['name'], user_profile['email'], wechat_id['answer'], hobbies['answer'])
-    except :
-        print i+1, user_profile['name'], user_profile['email'], first_time['answer'], add_quote(first_attend['answer']), status, date
-        str='%d,%s,%s\n' % (i+1, user_profile['name'], user_profile['email'])'''
+            output_file.write(response_event.json()["events"][j]["name"]["text"] + '\n')
 
 for i in range (response.json()["pagination"]["object_count"]):
   if (i<50):
