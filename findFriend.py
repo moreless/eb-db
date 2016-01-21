cb511ca2a7c84a342a6ea91a0fd8ec6c96d196ba
lanchao <lanchao>
2016-01-21 00:23:34 -0800

    Add the friend ranking function

--- findFriend.py
+++ findFriend.py
@@ -2,6 +2,7 @@ import numpy as np
 import pymongo
 from pymongo import MongoClient
 import operator
+from math import sqrt
         
 client = MongoClient('localhost', 27017)
 db = client['ValleyRain']
@@ -13,10 +14,57 @@ def findFriend(username):
         print 'User %s just attended once. Need more data.' %(username)
         return
 
-    similarityScore = {}
-    attendList = record['attend_times']
+    similarityScoreDict = {}
+    attendDict = record['attended']
+    normAttendDict = calNorm(attendDict)
+
     for entry in collection.find():
         if entry['attend_times'] <= 1:
-            pass    
-        parAttendList = entry['attended']
+            continue    
+        pairAttendDict = entry['attended']
+        normPairAttendDict = calNorm(pairAttendDict)
+        crossProduct = crossProd(attendDict, pairAttendDict)
+        similarityScore = crossProduct / (normAttendDict * normPairAttendDict)
+        similarityScoreDict[entry['name']] = similarityScore
+        
+    return sorted(similarityScoreDict.items(), key=operator.itemgetter(1), reverse=True)[0:-1]
+        
+def calNorm(dict):
+    res = 0;
+    for x in dict.keys():
+        if dict[x] == 'Checked In':
+            res = res + 1 * 1
+        elif dict[x] == 'Attending':
+            res = res + 0.5 * 0.5
+        elif dict[x] == 'Not Attending':
+            res = res + 0.25 * 0.25
+    return sqrt(res)    
+
+def crossProd(dict1, dict2):
+    res = 0
     
+    for x in dict1.keys():
+        a = 0
+        if x in dict2.keys():
+            b = 0
+            if dict1[x] == 'Checked In':
+                a = 1
+            elif dict1[x] == 'Attending':
+                a = 0.5
+            elif dict1[x] == 'Not Attending':
+                a = 0.25
+            
+            if dict2[x] == 'Checked In':
+                b = 1
+            elif dict2[x] == 'Attending':
+                b = 0.5
+            elif dict2[x] == 'Not Attending':
+                b = 0.25
+            
+            res = res + a * b
+    
+    return res
+            
+print findFriend('Haoting Luo')        
+            
+        
